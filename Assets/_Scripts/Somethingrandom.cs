f805bbf9ec5cad69a3f75c6ede1b8f2a5407d553
rge_laptop <rge@plan.aau.dk>
2019-04-26 22:55:16 +0200

    added greeting

--- Assets/_Scripts/Somethingrandom.cs
+++ Assets/_Scripts/Somethingrandom.cs
@@ -6,11 +6,6 @@ public class Somethingrandom : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        Debug.Log("Hello Wilma and Kikko");
 	}
 }
