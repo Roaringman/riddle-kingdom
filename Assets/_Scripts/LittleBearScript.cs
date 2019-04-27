56e602d8607d8673eb28b175a3ce8e62e4ae425c
DESKTOP-R38ODDL\Kikko <Kikkohenriksen@gmail.com>
2019-04-27 21:55:18 +0200

    Stuff

---
+++ Assets/_Scripts/LittleBearScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LittleBearScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    public void Sleep()
+    {
+
+        gameObject.GetComponent<Animator>().Play("Sleep"); 
+
+    }
+
+}
