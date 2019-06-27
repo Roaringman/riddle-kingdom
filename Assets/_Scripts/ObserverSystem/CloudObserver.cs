4629bb346c3b886981a5b4410b840a32cbda0e96
Roar Engell <rgengell@gmail.com>
2019-06-27 17:09:31 +0200

    hej

--- Assets/_Scripts/ObserverSystem/CloudObserver.cs
+++ Assets/_Scripts/ObserverSystem/CloudObserver.cs
@@ -11,19 +11,26 @@ namespace CommandPattern
         //What will happen when this box gets an event
         CloudEvents cloudEvent;
         GameObject rainCloudParent;
-        GameObject rainCloudChild;
+        RainCloud rainCloudChild;
+
+
+        int test = 0;
 
         public CloudObserver(GameObject cloudObj, CloudEvents cloudEvent)
         {
             this.cloudObj = cloudObj;
             this.cloudEvent = cloudEvent;
+            rainCloudParent = GameObject.Find("RainCloudParent");
+
+            rainCloudChild =  new RainCloud(rainCloudParent.transform.Find("cloud").gameObject);
+            Debug.Log(rainCloudChild.cloud.name);
         }
 
         public override void OnNotify(Vector3 avgPos)
         {
             float speed = cloudEvent.GetLerpSpeed();
-            rainCloudParent = GameObject.Find("RainCloudParent");
-            rainCloudChild = rainCloudParent.transform.Find("cloud").gameObject;
+
+            
 
             //rainCloudObj = rainCloudParent.transform.Find("RainCloud").gameObject;
 
@@ -31,27 +38,40 @@ namespace CommandPattern
             Vector3 endPos = avgPos;
 
             var seq = LeanTween.sequence();
-            seq.append(1f); // delay everything one second
+            seq.append(0.5f); // delay everything one second
             seq.append(LeanTween.move(this.cloudObj, endPos, speed).setEase(LeanTweenType.easeInQuint)); // do a tween
 
             seq.append(() =>
             { // fire event after tween
                 this.cloudObj.gameObject.SetActive(false);
+                rainCloudChild.merges = 1;
+                Debug.Log(rainCloudChild.merges);
+                tester();
             });
 
             seq.append(() => { // fire event after tween
-                if (!rainCloudChild.activeSelf)
+                if (rainCloudChild.activeState == false)
                 {
+                    Debug.Log(rainCloudChild.activeState);
                     rainCloudParent.transform.position = endPos;
                     rainCloudParent.transform.position += new Vector3(0,2f,0);
-                    rainCloudChild.SetActive(true);
+                    rainCloudChild.activeState = true;
                 } else
                 {
-                    rainCloudChild.transform.localScale += new Vector3(1f, 1f, 1f);
-                    rainCloudChild.transform.position += new Vector3(0, 1f, 0);
+                    Debug.Log(rainCloudChild.activeState);
+                    rainCloudChild.cloud.transform.localScale += new Vector3(1f, 1f, 1f);
+                    rainCloudChild.cloud.transform.position += new Vector3(0, 1f, 0);
                 }
             }); 
 
+
+
+        }
+
+        void tester()
+        {
+            test += test  + 1;
+            Debug.Log(test);
         }
     }
 }
